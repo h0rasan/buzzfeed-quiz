fe32b9d6bf3fb9f598dce0018703703bf8fdca3e
Seid Jasarevic <jasarevics@live.com>
2022-10-26 19:58:30 +0200

    setup json server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import Title from "./components/Title";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [quiz, setQuiz] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/quiz");
+      const json = await response.json();
+      setQuiz(json);
+    } catch (err) {}
+  };
   return (
     <div>
       <Title />
